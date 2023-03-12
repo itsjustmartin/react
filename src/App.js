b76e3559b06ef58f0a93a1f7fc24e814110a1c37
Martin <itsjutmartin>
2023-03-12 12:54:35 +0300

    calc

--- src/App.js
+++ src/App.js
@@ -1,51 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
-import MealsProvider from './provider/MealsProvider';
-import MealsList from './components/MeatList';
-import Counter from './components/Counter';
-import Wallet from './components/Wallet';
-import {Routes , Route ,Link} from "react-router-dom";
+import {
+  useState,
+  useRef
+} from "react"; 
+import "./App.css";
 
-function Bag(props) {
-  const bag = {
-      padding: "20px",
-      border: "1px solid gray",
-      background: "#fff",
-      margin: "20px 0"
-  }
-  return (
-      <div style={bag}> hi
-          {props.children}
-          {props.name}
-      </div>
-  )
-}
-
-function Avatar(props){
-  const userLogo =<img alt="alt text" src={logo} />
-  return userLogo;
-}
-
-function App() {
-  return (<div>
-    <nav>
-      <Link to="/" className='nav-item' >HomePage</Link>
-      <Link to="/avatar" className='nav-item' >Avatar</Link>
-      <Link to="/wallet" className='nav-item' >Wallet</Link>
-      <Link to="/mealsMenu" className='nav-item' >Meals Menu</Link>
-    </nav>
-    <Routes>
-      <Route path='/' element={<Bag children={<h3>Hello there</h3>} name="martin">child</Bag>}></Route>
-      <Route path='/avatar' element={<Avatar />}></Route>
-      <Route path='/wallet' element={<Wallet/>}></Route>
-      <Route path='/mealsMenu' element={
-        <MealsProvider>
-          <MealsList />
-          <Counter />
-        </MealsProvider>
-      }></Route>
-    </Routes>
-  </div>);
-};
-
-export default App;
+function App() { 
+  const inputRef = useRef(null); 
+  const resultRef = useRef(null);
+  const [result, setResult] = useState(null);
+  const [inputValue, setInputValue] = useState(null);
+  
+  
+  function plus(e) { 
+    e.preventDefault(); 
+    setResult((result) => result + Number(inputValue)); 
+  }; 
+ 
+  function minus(e) { 
+  	e.preventDefault(); 
+    setResult((result) => result - Number(inputValue));
+  };
+ 
+  function times(e) { 
+    e.preventDefault(); 
+    setResult((result) => result * Number(inputValue)); 
+  }; 
+ 
+  function divide(e) { 
+    e.preventDefault(); 
+    setResult((result) => result / Number(inputValue));
+  };
+ 
+  function resetInput(e) {
+    e.preventDefault(); 
+    setInputValue(null);
+    inputRef.current.value = 0;
+  }; 
+ 
+  function resetResult(e) { 
+  	e.preventDefault(); 
+    setResult(null);
+    
+  }; 
+ 
+  return ( 
+    <div className="App">
+      <div> 
+        <h1>Simplest Working Calculator</h1> 
+      </div> 
+      <form> 
+        <p ref={resultRef}> 
+          total : {result}
+        </p> 
+        <input
+          pattern="[0-9]" 
+          ref={inputRef}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          type="number" 
+          placeholder="Type a number" 
+        /> 
+        <button onClick={plus}>plus</button> 
+        <button onClick={minus}>minus</button>
+        <button onClick={times}>times</button>
+        <button onClick={divide}>divide</button><br/>
+        <button onClick={resetInput}>resetInput</button>
+        <button onClick={resetResult}>resetResult</button>
+        
+      </form> 
+    </div> 
+  ); 
+} 
+ 
+export default App; 
