90e0cb2f730e7d123b5f05ef476ac1a453c5a379
Martin <itsjutmartin>
2023-03-12 14:39:52 +0300

    creat list map filter sort

--- src/App.js
+++ src/App.js
@@ -1,75 +1,36 @@
-import {
-  useState,
-  useRef
-} from "react"; 
 import "./App.css";
+import DessertsList from "./DessertsList";
 
-function App() { 
-  const inputRef = useRef(null); 
-  const resultRef = useRef(null);
-  const [result, setResult] = useState(null);
-  const [inputValue, setInputValue] = useState(null);
-  
-  
-  function plus(e) { 
-    e.preventDefault(); 
-    setResult((result) => result + Number(inputValue)); 
-  }; 
- 
-  function minus(e) { 
-  	e.preventDefault(); 
-    setResult((result) => result - Number(inputValue));
-  };
- 
-  function times(e) { 
-    e.preventDefault(); 
-    setResult((result) => result * Number(inputValue)); 
-  }; 
- 
-  function divide(e) { 
-    e.preventDefault(); 
-    setResult((result) => result / Number(inputValue));
-  };
- 
-  function resetInput(e) {
-    e.preventDefault(); 
-    setInputValue(null);
-    inputRef.current.value = 0;
-  }; 
- 
-  function resetResult(e) { 
-  	e.preventDefault(); 
-    setResult(null);
-    
-  }; 
- 
-  return ( 
+const desserts = [
+  {
+    name: "Chocolate Cake",
+    calories: 400,
+    createdAt: "2022-09-01",
+  },
+  {
+    name: "Ice Cream",
+    calories: 200,
+    createdAt: "2022-01-02",
+  },
+  {
+    name: "Tiramisu",
+    calories: 300,
+    createdAt: "2021-10-03",
+  },
+  {
+    name: "Cheesecake",
+    calories: 600,
+    createdAt: "2022-01-04",
+  },
+];
+
+function App() {
+  return (
     <div className="App">
-      <div> 
-        <h1>Simplest Working Calculator</h1> 
-      </div> 
-      <form> 
-        <p ref={resultRef}> 
-          total : {result}
-        </p> 
-        <input
-          pattern="[0-9]" 
-          ref={inputRef}
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          type="number" 
-          placeholder="Type a number" 
-        /> 
-        <button onClick={plus}>plus</button> 
-        <button onClick={minus}>minus</button>
-        <button onClick={times}>times</button>
-        <button onClick={divide}>divide</button><br/>
-        <button onClick={resetInput}>resetInput</button>
-        <button onClick={resetResult}>resetResult</button>
-        
-      </form> 
-    </div> 
-  ); 
-} 
- 
-export default App; 
+      <h2>List of low calorie desserts:</h2>
+      <DessertsList data={desserts} />
+    </div>
+  );
+}
+
+export default App;
