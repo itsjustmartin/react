e55ead0cdb60f962175fcde18edaf8cf84cc5c1e
Martin <itsjutmartin>
2023-03-14 12:13:36 +0300

    gift Card

--- src/App.js
+++ src/App.js
@@ -1,55 +1,48 @@
-import "./App.css";
-import React from "react";
+import { useState } from "react";
 
-function GoalFoarm(props){
-  const [formData,setFormData] = React.useState({goal :"",by :""});
-  
-  function changeHandler(e){
-    setFormData({...formData,[e.target.name]:e.target.value});
-  } 
+export default function App() {
+  const [giftCard, setGiftCard] = useState(
+    {
+        firstName: "Jennifer",
+        lastName: "Smith",
+        text: "Free dinner for 4 guests",
+        valid: true,
+        instructions: "To use your coupon, click the button below.",
+    }
+  );
 
-  function submitHandler(e){
-    e.preventDefault();
-    props.onAdd(formData);
-    setFormData({goal : "" ,by :""});
+  function spendGiftCard() {
+    setGiftCard(prevState => {
+        return {
+          ...prevState,
+          text: "Your coupon has been used.",
+          valid: false,
+          instructions: "Please visit our restaurant to renew your gift card.",
+        }
+    });
   }
 
   return (
-    <>
-    <h1>my Goals :</h1>
-    <form onSubmit={submitHandler}>
-      <input type="text" name="goal" placeholder="Goal" value={formData.goal} onChange={changeHandler} />
-      <input type={"text"} name="by" placeholder="By .." value={formData.by} onChange={changeHandler} />
-      <button>submit Goal</button>
-    </form>
-    </>
-  );
-}
-
-function ListOfGoals(props){
-  return (
-    <ul>
-      {props.allGoals.map((g)=>(
-        <li key={g.goal}> 
-        <span>My goal is to {g.goal} by {g.by}</span>
-        </li>
-      ))}
-    </ul>
-  )
-}
-
-function App() {
-  const [allGoals,updateAllGoals] = React.useState([{goal :"first",by :"anymo"}]);
-
-  function addGoal(goal){updateAllGoals([...allGoals,goal]);}
-
-  return (
-    <div className="App">
-      <GoalFoarm onAdd={addGoal} />
-      <ListOfGoals allGoals={allGoals} />
+    <div style={{padding: '40px'}}>
+      <h1>
+        Gift Card Page
+      </h1>
+      <h2>
+        Customer: {giftCard.firstName} {giftCard.lastName}
+      </h2>
+      <h3>
+        {giftCard.text}
+      </h3>
+      <p>
+        {giftCard.instructions}
+      </p>
+      {
+        giftCard.valid && (
+          <button onClick={spendGiftCard}>
+            Spend Gift Card
+          </button>
+        )
+      }
     </div>
   );
 }
-
-
-export default App;
