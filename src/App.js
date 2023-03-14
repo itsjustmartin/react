1ea099a53a22bee08a84ee5350c861e93c18a72b
Martin <itsjutmartin>
2023-03-14 14:45:14 +0300

    fetching from API

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
-import React from "react";
-import "./App.css";
-
+import React, { useState, useEffect } from "react";
 
 export default function App() {
-  const [toggle,setToggle] = React.useState(false);
+  const [btcData, setBtcData] = useState({});
 
-  const clickHandler = ()=>{
-    setToggle(!toggle);
-  }
-  // just one time invoke
-  // React.useEffect(()=>{
-  //   document.title = toggle ? "welcome to Little Lemon" : "using Side Effect" 
-  // },[]);
-  // evrey time toggle change 
-  React.useEffect(()=>{
-    document.title = toggle ? "welcome to Little Lemon" : "using Side Effect" 
-  },[toggle]);
+  const fetchData = () => {
+    fetch(`https://api.coindesk.com/v1/bpi/currentprice.json`)
+      .then((response) => response.json())
+      .then((jsonData) => setBtcData(jsonData.bpi.USD))
+      .catch((error) => console.log(error));
+  };
 
-  // React.useEffect(()=>{
-  //   document.title = toggle ? "welcome to Little Lemon" : "using Side Effect" 
-  // });
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  return (
-    <div>
-      <h1> using the useEffect hook</h1>
-      <button onClick={clickHandler}>Toggle Message</button>
-      {toggle && <h2>welcome to Little Lemon</h2>}
+  const [user , setUser] = React.useState([]);
+
+  const fetchData2 = () => {
+    fetch("https://randomuser.me/api/?reults=1")
+      .then((response) => response.json())
+      .then(data => setUser(data));
+  };
+
+  useEffect(() => {
+    fetchData2();
+  }, []);
+
+  const USD = () =>{
+    return (
+      <div>
+    <>
+      <h1>Current BTC/USD data</h1>
+      <p>Code: {btcData.code}</p>
+      <p>Symbol: {btcData.symbol}</p>
+      <p>Rate: {btcData.rate}</p>
+      <p>Description: {btcData.description}</p>
+      <p>Rate Float: {btcData.rate_float}</p>
+    </>
     </div>
-  )
+    )
+  }
+  return Object.keys(user).length > 0 ? (<div>
+    <h1>Data retured</h1>
+    <h2>First name : {user.results[0].name.first}</h2>
+    <h2>LAst name : {user.results[0].name.last}</h2>
+    <br></br>
+    <USD/>
+  </div>
+  ) : <h1>DAta pending</h1>
 }
